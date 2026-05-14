08927962c92bbc4f47b661ff55de50206f2cf4ea
mamuciormamut-del <mamucior.mamut@gmail.com>
2026-05-14 10:15:24 +0200

    poprawki

--- lib/webcontainer/vite-react-starter.ts
+++ lib/webcontainer/vite-react-starter.ts
@@ -78,6 +78,7 @@ const INDEX_HTML = `<!doctype html>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>wybitnastrona.pl - preview</title>
+    <link rel="canonical" href="https://wybitnastrona.pl" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
 ${ELEMENT_PICKER_SCRIPT}
